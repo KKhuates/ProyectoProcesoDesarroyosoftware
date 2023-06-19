8794b7a0d517fb9ade0051fdcc4b355c5510bf83
KKhuates <salvadortes05@gmail.com>
2023-06-18 21:23:47 -0400

    update

--- src/rutas/customer.js
+++ src/rutas/customer.js
@@ -4,12 +4,16 @@ const customerController = require('../controladores/customerController');
 const { upload } = require('../controladores/customerController');
 
 router.get('/', customerController.mostrarInicio);
-router.get('/menuprincipal', customerController.mostrarMenuPrincipal);
-router.get('/paginaerror', customerController.paginaError);
-router.get('/login', customerController.iniciarSesion);
+router.get('/login', customerController.mostrarFormularioLogin);
 router.get('/registros', customerController.registrarUsuario);
 router.get('/subir_consultoria', customerController.mostrarFormularioSubirConsultoria);
+router.get('/Inicio_admin', customerController.obtenerUsuariosConConsultorias);
+router.get('/Inicio_admin', customerController.mostrarInicioAdmin);
+router.get('/Inicio_estudiante', customerController.mostrarInicioEstudiante);
+router.get('/Inicio_cimite',customerController.mostrarInicioComite);
 
+
+router.post('/eliminarUsuario/:rut', customerController.eliminarUsuario);
 router.post('/login', customerController.iniciarSesion);
 router.post('/registros', customerController.registrarUsuario);
 router.post('/subir_consultoria', upload.single('file'), customerController.mostrarFormularioSubirConsultoria);
